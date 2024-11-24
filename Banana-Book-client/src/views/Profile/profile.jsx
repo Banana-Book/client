ba5f7cd1fdcf4cd28abc893fde8be18b7d10b3be
MelvinDiaz <melvin.diazayala@proton.me>
2024-11-23 22:38:51 -0600

    Add: responsive to my profile

--- Banana-Book-client/src/views/Profile/profile.jsx
+++ Banana-Book-client/src/views/Profile/profile.jsx
@@ -1,19 +1,18 @@
 import './profile.css';
-import UserInfo from "./Components/UserInfo/UserInfo";
-import UserPic from "./Components/UserPic/UserPic";
-import UserPosts from "./Components/UserPosts/UserPosts";
+import UserInfo from './Components/UserInfo/UserInfo';
+import UserPic from './Components/UserPic/UserPic';
+import UserPosts from './Components/UserPosts/UserPosts';
 
 const Profile = () => {
-
-    return (
-        <section className="profile">
-            <div className="info">
-                <UserInfo />
-                <UserPic />
-            </div>
-            <UserPosts />
-        </section>
-    );
+  return (
+    <section className="profile">
+      <div className="info">
+        <UserPic />
+        <UserInfo />
+      </div>
+      <UserPosts />
+    </section>
+  );
 };
 
 export default Profile;
